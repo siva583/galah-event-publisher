1ca33cb07742b7307c7a946150793ac9acd51af4
sgamall <sgamall@92.242.140.2>
2017-12-18 00:41:36 -0600

    added controller advice and removed unneccessary classes

---
+++ src/main/java/com/intuit/galah/exception/EventConsumerExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.intuit.galah.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class EventConsumerExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	
+	@ExceptionHandler(value = {Exception.class})
+	@ResponseBody
+	public ResponseEntity<?> handleAnyException(Exception e) {
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+}
