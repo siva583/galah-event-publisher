4ac25910bc33e323065917cf216a7df808572e2b
sgamall <sgamall@92.242.140.2>
2017-12-18 00:21:56 -0600

    modified controller to return error message

--- src/main/java/com/intuit/galah/config/CorsFilter.java
+++ src/main/java/com/intuit/galah/config/CorsFilter.java
@@ -7,19 +7,21 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Configuration
 public class CorsFilter extends OncePerRequestFilter {
-  //  private static final Logger LOG = LoggerFactory.getLogger(CorsFilter.class);
+    private static final Logger LOG = LoggerFactory.getLogger(CorsFilter.class);
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         response.addHeader("Access-Control-Allow-Origin", "*");
         if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
-          //  LOG.trace("Sending Header....");
+            LOG.trace("Sending Header....");
             response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
             response.addHeader("Access-Control-Allow-Headers", "Content-Type");
             response.addHeader("Access-Control-Max-Age", "1");
--- src/main/java/com/intuit/galah/controller/EventPublishController.java
+++ src/main/java/com/intuit/galah/controller/EventPublishController.java
@@ -1,6 +1,5 @@
 package com.intuit.galah.controller;
 
-import java.util.Date;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
@@ -10,49 +9,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.intuit.galah.iface.IEventPublisherService;
-import com.intuit.galah.model.User;
 
 
 @RestController
 public class EventPublishController {
-	
+
 	@Autowired
 	IEventPublisherService service;
-	
-	@GetMapping(value = "/test", produces = {MediaType.TEXT_PLAIN_VALUE})
-	public String test(@RequestBody User user) {
-		try {
-			service.publishMessages("test", "Hi from Galah!");
-		} catch (InterruptedException | ExecutionException | TimeoutException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return "Success";
-	}
-	
+
 	@PostMapping(value = "/publish/{topicName}", produces = { MediaType.APPLICATION_JSON_VALUE }, consumes = { MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<Object> publishToTopic(@Valid @PathVariable String topicName, @Valid @RequestBody Object message) {
-		//validate
-		//send - success 
-		//send - fault
-		//excption handling - ControllerAdvice
-		//unit test cases
+	public ResponseEntity<?> publishToTopic(@Valid @PathVariable String topicName, @Valid @RequestBody Object message) {
 		try {
 			service.publishMessages(topicName, message);
+			return new ResponseEntity<>("Success", HttpStatus.OK);
 		} catch (InterruptedException | ExecutionException | TimeoutException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		return new ResponseEntity<>("Success", HttpStatus.OK);
-		
 	}
-
 }
